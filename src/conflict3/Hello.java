2f95a8246efaa145c049c8e75b73aa9381071c0d
pushkarsa <pushkar04cse@gmail.com>
2020-01-26 01:48:48 +0530

    asd

--- src/conflict3/Hello.java
+++ src/conflict3/Hello.java
@@ -4,12 +4,16 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("adding in conflict3");
-
 		System.out.println("first");
-
 		System.out.println("second");
+		System.out.println("khusbu did");
+		
+		hello();
 
-
+	}
+	
+	public static void hello() {
+		System.out.println("runing hello mehtods");
 	}
 
 }
