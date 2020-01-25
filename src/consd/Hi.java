9a18423ac7ca3ca5683a3a5b3fb966e5ef274b1f
pushkarsa <pushkar04cse@gmail.com>
2020-01-26 01:48:57 +0530

    as

---
+++ src/consd/Hi.java
@@ -0,0 +1,13 @@
+package consd;
+
+import conflict3.Hello;
+
+public class Hi {
+	
+	public static void main(String[] args) {
+		System.out.println("hi");
+		System.out.println("thsdfaere");
+		
+		Hello.hello();
+	}
+}
